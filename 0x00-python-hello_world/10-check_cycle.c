e811ebda8536cc478267b4a96a202ab93d73cf09
Prish20 <prish@students.uonbi.ac.ke>
2023-11-28 02:34:46 +0300

    updates

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,7 +4,9 @@
  * check_cycle - checks if a singly linked list has a cycle in it
  * @list: pointer to head of list
  * Return: 0 if there is no cycle, 1 if there is a cycle
- */
+ * Description: This function checks if a singly linked list has a cycle in it
+ * using Floyd's cycle-finding algorithm.
+*/
 int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
