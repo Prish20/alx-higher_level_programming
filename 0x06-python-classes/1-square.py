a8677aed44d4b293cede9baa833165b204b671b4
Prish20 <prish@students.uonbi.ac.ke>
2024-01-21 20:21:46 +0000

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+class Square:
+    """This is the Square class which defines a square by its size"""
+
+    def __init__(self, size):
+        """
+        This is the constructor method for the Square class.
+        It defines a square by its size.
+        """
+        self.__size = size
