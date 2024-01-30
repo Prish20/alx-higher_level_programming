039cb40e936414a4643661cdd2fb49c14d9e0f41
Prish20 <prish@students.uonbi.ac.ke>
2024-01-30 16:48:20 +0000

    Task 2-Fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-
-"""Rectangle module"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-
-    """Rectangle class"""
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
+        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -25,6 +30,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get/set the height of the rectangle."""
         return self.__height
 
     @height.setter
@@ -34,12 +40,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        try:
-            myrectangle = Rectangle(2, "3")
-        except Exception as e:
-            print("[{}] {}".format(e.__class__.__name__, e))
-
-        try:
-            myrectangle = Rectangle("2", 3)
-        except Exception as e:
-            print("[{}] {}".format(e.__class__.__name__, e))
